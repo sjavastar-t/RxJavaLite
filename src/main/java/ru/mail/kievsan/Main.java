9ef4054ed198ae054becef38fa228e4e1023a3be
Киевский Сергей Николаевич <kievsan@mail.ru>
2025-06-16 07:04:32 +0300

    Init

--- src/main/java/ru/mail/kievsan/Main.java
+++ src/main/java/ru/mail/kievsan/Main.java
@@ -1,21 +1,17 @@
 package ru.mail.kievsan;
 
-
-import ru.mail.kievsan.rx.core.Disposable;
 import ru.mail.kievsan.rx.core.Observable;
 import ru.mail.kievsan.rx.operators.FilterOperator;
+import ru.mail.kievsan.rx.operators.FlatMapOperator;
 import ru.mail.kievsan.rx.operators.MapOperator;
 import ru.mail.kievsan.rx.schedulers.ComputationScheduler;
-import ru.mail.kievsan.rx.schedulers.IOScheduler;
 import ru.mail.kievsan.rx.schedulers.Scheduler;
-
-import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("\n\tmap, filter:\t");
         try (Scheduler scheduler = new ComputationScheduler()) {
+            System.out.println("\n\t map, filter:");
             Observable<Integer> source = Observable.range(1, 10);
             FilterOperator.apply(MapOperator.apply(source, i -> i * 10), i -> i > 20)
 //                    .subscribeOn(scheduler
@@ -26,10 +22,15 @@ public class Main {
                             () -> System.out.println("Completed")
                     );
             Thread.sleep(1000);
+
+            System.out.println("\n\t flatMap:");
+            FlatMapOperator.apply(source, i -> Observable.just(i, i * 10))
+                    .subscribe(i -> System.out.println("flatMap: " + i));
+            Thread.sleep(1000);
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 
 }
