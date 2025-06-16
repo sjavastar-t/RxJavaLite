16fde3bd2b2d4da544ee5407b27b9d1171584ca5
Киевский Сергей Николаевич <kievsan@mail.ru>
2025-06-16 06:05:06 +0300

    Init

--- src/main/java/ru/mail/kievsan/Main.java
+++ src/main/java/ru/mail/kievsan/Main.java
@@ -1,9 +1,26 @@
 package ru.mail.kievsan;
 
 
-public class Main {
-    public static void main(String[] args) {
+import ru.mail.kievsan.rx.core.Observable;
+import ru.mail.kievsan.rx.operators.FilterOperator;
+import ru.mail.kievsan.rx.operators.MapOperator;
+import ru.mail.kievsan.rx.schedulers.ComputationScheduler;
+import ru.mail.kievsan.rx.schedulers.IOScheduler;
 
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("\n\tmap, filter:\t");
+        Observable<Integer> source = Observable.range(1, 10);
+        FilterOperator.apply(MapOperator.apply(source, i -> i * 10), i -> i > 20)
+                .subscribeOn(new IOScheduler())
+                .observeOn(new ComputationScheduler())
+                .subscribe(
+                        i -> System.out.println("Received... " + i),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Completed")
+                );
+        Thread.sleep(2000);
 
     }
 
